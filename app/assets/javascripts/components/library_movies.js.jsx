f4b5e656f9bb6c812417c17ce8042179628b677d
J Shin <jjshin85@gmail.com>
2016-04-13 18:44:29 -0400

    fix routing url issue for user libraries

---
+++ app/assets/javascripts/components/library_movies.js.jsx
@@ -0,0 +1,49 @@
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
+  }
+};
+
+
+var LibraryMovie = React.createClass({
+
+  mixins: [SetIntervalMixin],
+  getInitialState: function(){
+    return {seconds: 0,  library: this.props.library, user_id: this.props.user_id};
+  },
+
+  componentDidMount: function(){
+    this.setInterval(this.libraryMovies, 5000);
+  },
+
+  libraryMovies: function(){
+    $.ajax({
+      url: "/users/" + this.props.user_id + "/libraries/" + this.props.id,
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: function(data){
+        console.log(data);
+        this.setState({libraryMovies: data});
+      }.bind(this)
+    });
+  },
+
+  render: function() {
+    return (<ul>{this.state.libraryMovies.map(function (key, value){
+        return <li className="movie_div" key={key.id} >
+          <p className='movie_title'>{key.title}</p>
+          <p className="movie_description">{key.description}</p>
+          <p className='movie_release_year'>{key.release_year}</p>
+          <img className='movie_image' src={key.image_url}/>
+        </li>;
+      })}
+    </ul>)
+  }
+});
