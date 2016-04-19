d26ca11639effd2d80953273448a2d7cd15913ca
J Shin <jjshin85@gmail.com>
2016-04-19 19:52:38 -0400

    add images to movie get requests

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,20 +1,28 @@
 var BASE_URL = "https://api.themoviedb.org/3";
-var BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w500";
-var API_KEY = ENV["API_KEY"];
-
+var BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
+var API_KEY;
+var query = "?query=toy+story"
 $(document).ready(function(){
-    $.get(BASE_URL + "/search/movie" + API_KEY, onSuccess);
+    var image_size = "w92"
+    API_KEY = "";
+    var url = BASE_URL + "/search/movie" +  query + "&" + API_KEY;
+    $.get(url, onSuccess);
 
-    var image_url = BASE_IMAGE_URL + "/6bCplVkhowCjTHXWv49UjRPn0eK.jpg";
+    var image_url = BASE_IMAGE_URL + image_size + "/6bCplVkhowCjTHXWv49UjRPn0eK.jpg";
 
-    $('.test').append('<img src="' + image_url + '">');
+
+
+    $.get(image_url, function(data){
+    });
 });
 
 function onSuccess(data){
   console.log(data);
+  console.log(data[0]);
   data.results.forEach(function(element){
     console.log(element.original_title);
     console.log(element.overview);
+    $('.test').append('<img src=' + image_url + '>');
   })
   $('.test').append();
 }
