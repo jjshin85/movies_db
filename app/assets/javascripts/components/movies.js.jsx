a12d8e350dd114a9c36cfe3b5907085015a5f75d
J Shin <jjshin85@gmail.com>
2016-05-02 16:28:15 -0400

    fix styling for columns for all device sizes

--- app/assets/javascripts/components/movies.js.jsx
+++ app/assets/javascripts/components/movies.js.jsx
@@ -44,15 +44,18 @@ var Movie = React.createClass({
   {
     return (<div className='container'>{this.state.movies.map(function (key, value)
       {
-        // col-xs-7, col-sm-6 col-md-4
-      return <div className="col-md-4" key={key.id} >
+        return <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12" key={key.id} >
                 <div className='movie'>
                   <div className='background_overlay'></div>
                   <div className='image_container'>
                     <img className='movie_image' src={ BASE_IMAGE_URL + image_size_sm + key.poster_path }/>
                   </div>
-                  <p className="movie_overview">{key.overview}</p>
-                  <h4 className='movie_title'>{key.title}</h4>
+                  <div className='text_container'>
+                    <p className="movie_overview">{key.overview}</p>
+                    <div className="title_area">
+                      <h4 className='movie_title'>{key.title}</h4>
+                    </div>
+                  </div>
                 </div>
               </div>;
       })}
--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -11,18 +11,17 @@ function onMovieHover(){
       var overview = movie.children('.movie_overview');
       var image_container = movie.children('.image_container');
       // var image = movie.children('.movie_image');
+      var text_container = movie.children('.text_container');
       var image = movie.children('.image_container').children('img');
       var overlay = movie.children('.background_overlay');
       if(e.type == 'mouseenter')
       {
-        title.css('display', 'inherit');
-        overview.css('display', 'inherit');
+        text_container.css('display', 'inherit');
         image.css('opacity', .5);
         overlay.css('opacity', .5);
       }
       else{
-        title.css('display', 'none');
-        overview.css('display', 'none');
+        text_container.css('display', 'none');
         image.css('opacity', 1);
         overlay.css('opacity', 0);
       }
