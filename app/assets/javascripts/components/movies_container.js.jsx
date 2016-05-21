86e68ea6f2debfdf0b8e88e52ab7feadbc8bb6fb
J Shin <jjshin85@gmail.com>
2016-05-21 15:32:04 -0700

    add search functionality to '/movies' page

--- app/assets/javascripts/components/movies_container.js.jsx
+++ app/assets/javascripts/components/movies_container.js.jsx
@@ -1,43 +1,32 @@
 var MoviesContainer = React.createClass({
   getInitialState: function() {
-    return { query: '' }
+    return { movies: this.props.movies }
   },
   setSearchQuery: function(input) {
-    this.setState({ query: input });
+    this.searchMovies(input);
   },
-  // handleSearchClick: function() {
-  //
-  // },
+  searchMovies: function(input){
+    var query = input.replace(" ", "+");
+    console.log(query);
+    var searchURL = "/movies/?query=" + query;
+    console.log(searchURL);
+    $.ajax({
+      url: searchURL,
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: function(data){
+        console.log(data)
+        this.setState({ movies: data.movies });
+      }.bind(this)
+    });
+  },
 
   render: function(){
     return (<div>
         <p>from container, query: {this.state.query}</p>
         <SearchBar setQuery={this.setSearchQuery}/>
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={this.state.movies} />
       </div>);
-  }
-});
-
-
-var SearchBar = React.createClass({
-  getInitialState: function(){
-      return { userInput: '' }
-  },
-  updateSearchQuery: function(e){
-      this.setState({ userInput: e.target.value });
-  },
-  handleSetQuery: function(){
-      this.props.setQuery(this.state.userInput);
-      this.setState({ userInput: '' });
-  },
-  render: function(){
-    return (<div id="searchBar">
-          <input type="text" value={this.state.userInput} onChange={this.updateSearchQuery} />
-          <button onClick={this.handleSetQuery}>Search</button>
-          <p>test</p>
-          <p>partial: {this.state.userInput}</p>
-          // <p>final: {this.state.query}</p>
-        </div>
-    );
   }
 });
