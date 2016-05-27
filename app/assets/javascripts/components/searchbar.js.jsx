a6fb5041e672eb0a04c8809063df12de5fbfa820
J Shin <jjshin85@gmail.com>
2016-05-26 18:58:27 -0700

    make ever so slight adjustment to login form styling

--- app/assets/javascripts/components/searchbar.js.jsx
+++ app/assets/javascripts/components/searchbar.js.jsx
@@ -14,12 +14,20 @@ var SearchBar = React.createClass({
       <div className="col-lg-8 col-md-8 col-sm-8" id="searchbar">
         <div className="input-group">
           <input type="text"
-            id="searchbox"
+            id="username"
+            name='username'
             className="form-control"
             placeholder="Search for movie..."
             value={this.state.userInput}
             onChange={this.updateSearchQuery}
             />
+          <input type="password"
+              id="searchbox"
+              className="form-control"
+              placeholder="Search for movie..."
+              value={this.state.userInput}
+              onChange={this.updateSearchQuery}
+              />
           <span className="input-group-btn">
             <button className="btn btn-default"
               id="search-button"
