be850d97ef7f512a27b898f3fc3597d7348345e0
J Shin <jjshin85@gmail.com>
2016-06-14 23:21:49 -0700

    change movie images from poster image to backdrop image

--- app/assets/javascripts/components/movie.js.jsx
+++ app/assets/javascripts/components/movie.js.jsx
@@ -1,13 +1,10 @@
-var BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
-var image_size_sm = "w300";
-
 var Movie = React.createClass({
     render: function(){
-      return (<div className="col-lg-3 col-md-4 col-sm-6 col-xs-12" key={this.props.id} >
+      return (<div className="col-lg-4 col-md-6 col-sm-12 col-xs-12" key={this.props.id} >
                 <div className='movie'>
                   <div className='background_overlay'></div>
                   <div className='image_container'>
-                    <MovieImage posterPath={this.props.poster_path} />
+                    <MovieImage backdropPath={this.props.backdrop_path} />
                   </div>
                   <div className='text_container'>
                     <h4 className='movie_title'>{this.props.title}</h4>
