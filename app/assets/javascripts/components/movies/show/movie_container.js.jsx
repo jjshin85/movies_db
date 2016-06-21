470465a2e089d1ee1d2fe0f2acd15990cbd32739
J Shin <jjshin85@gmail.com>
2016-06-21 00:22:54 -0700

    add cast, crew, and people pages

--- app/assets/javascripts/components/movies/show/movie_container.js.jsx
+++ app/assets/javascripts/components/movies/show/movie_container.js.jsx
@@ -1,53 +1,10 @@
 var MovieContainer = React.createClass({
     getInitialState: function(){
-      return { movie: this.props.movie,
+      return {
+               movie: this.props.movie,
                cast: this.props.credits.cast.slice(0,4),
                crew: this.props.credits.crew.slice(0,4),
-               showFullCast: false,
-               showFullCrew: false}
-    },
-
-    setCredits: function() {
-      if(this.state.showFullCrew) {
-        this.state.crew = this.props.credits.crew;
-      }
-      else if(!this.state.showFullCrew) {
-        this.state.crew = this.props.credits.crew.slice(0,4);
-      }
-      else if(this.state.showFullCast) {
-        this.state.crew = this.props.credits.cast;
-      }
-      else if(!this.state.showFullCast) {
-        this.state.cast = this.props.credits.cast.slice(0,4);
-      }
-    },
-
-    setCreditsShowState: function(e) {
-      var id = e.target.gettributes.id;
-      var val = e.target.getAttribute('id');
-      console.log(val);
-      console.log(e.target);
-      if(id == 'id="cast-toggle"'){
-        console.log("what what");
-        console.log(this.state.showFullCast);
-        if(this.state.showFullCast){
-          this.state.showFullCast = false;
-        }
-        else {
-          this.state.showFullCast = true;
-        }
-        this.setCredits();
-      }
-      else {
-        if(this.state.showFullCrew){
-          this.state.showFullCrew = false;
-        }
-        else {
-          this.state.showFullCrew = true;
-        }
-        this.setCredits();
-      }
-      console.log(this.state.showFullCast);
+              }
     },
 
     render: function(){
@@ -68,12 +25,12 @@ var MovieContainer = React.createClass({
             <ul className='credits-list'>
               <p>Cast:</p>
               <CreditList credits={this.state.cast} type="cast" showall='false'/>
-              <button className='more-info-toggle-button' id='cast-toggle' onClick={this.setCreditsShowState}>+</button>
-            </ul>
+              <a href={"/movies/" + this.props.movie.id + "/cast" }><button>Explore full cast</button></a>
+          </ul>
             <ul className='credits-list'>
               <p>Crew:</p>
               <CreditList credits={this.state.crew} type="crew" showall='false'/>
-              <button className='more-info-toggle-button' id='crew-toggle' onClick={this.setCreditsShowState}>+</button>
+              <a href={"/movies/" + this.props.movie.id + "/crew" }><button>Explore full crew</button></a>
             </ul>
           </div>
         </div>
